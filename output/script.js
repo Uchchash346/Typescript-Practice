41f90cd1cb0f98f58e0478d3e516dbecb4bf3749
Uchchash346 <mru416@gmail.com>
2022-02-25 08:44:31 +0600

    module system

--- output/script.js
+++ output/script.js
@@ -1,18 +1,7 @@
-"use strict";
-var Player = /** @class */ (function () {
-    function Player(n, a, c) {
-        this.name = n;
-        this.age = a;
-        this.country = c;
-    }
-    Player.prototype.play = function () {
-        console.log(this.name + " from " + this.country + " is playing!");
-    };
-    return Player;
-}());
-var mashrafi = new Player('Mashrafi', 40, 'Bangladesh');
-var sakib = new Player('Sakib', 38, 'Bangladesh');
-console.log(mashrafi);
-var players = [];
+import { Player } from './classes/Player.js';
+const mashrafi = new Player('Mashrafi', 40, 'Bangladesh');
+const sakib = new Player('Sakib', 38, 'Bangladesh');
+console.log(mashrafi.age);
+const players = [];
 players.push(sakib);
 players.push(mashrafi);
--- src/script.ts
+++ src/script.ts
@@ -1,21 +1,9 @@
-class Player {
-    name: string;
-    age: number;
-    country: string;
-
-    constructor(n: string, a: number, c: string) {
-        this.name = n;
-        this.age = a;
-        this.country = c;
-    }
-    play() {
-        console.log(`${this.name} from ${this.country} is playing!`);
-    }
-}
+import { Player } from './classes/Player.js';
 
 const mashrafi = new Player('Mashrafi', 40, 'Bangladesh');
 const sakib = new Player('Sakib', 38, 'Bangladesh');
-console.log(mashrafi)
+
+console.log(mashrafi.age)
 const players: Player[] = [];
 
 players.push(sakib);
